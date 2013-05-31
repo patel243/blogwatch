d843af26d1fa97d7bb0d30a9ed9fe516a6d4bc51
Eugen <eparaschiv@optaros.com>
2013-05-31 13:06:35 +0300

    minor cleanup

--- src/main/java/org/baeldung/base/BlogBaseDriver.java
+++ src/main/java/org/baeldung/base/BlogBaseDriver.java
@@ -1,17 +1,17 @@
 package org.baeldung.base;
 
 import org.openqa.selenium.WebDriver;
-import org.selenium.base.AbstractDriverNew;
+import org.selenium.base.AbstractDriver;
 import org.selenium.base.SmokeUriUtil;
-import org.selenium.event.DriverChangeEventNew;
+import org.selenium.event.DriverChangeEvent;
 import org.selenium.spring.SpringContext;
 
-public abstract class BlogBaseDriver<D extends AbstractDriverNew<D>> extends AbstractDriverNew<D> {
+public abstract class BlogBaseDriver<D extends AbstractDriver<D>> extends AbstractDriver<D> {
     public BlogBaseDriver(final WebDriver driverToSet) {
         super(driverToSet);
 
         if (SpringContext.context() != null) {
-            SpringContext.context().publishEvent(new DriverChangeEventNew(this, this));
+            SpringContext.context().publishEvent(new DriverChangeEvent(this, this));
         }
     }
 
