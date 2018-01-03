ab41a2a18922c05f647836db21164071819897b0
asif-anwar <asifanwar451@gmail.com>
2018-01-03 18:31:50 +0400

    clean-up

--- src/test/java/com/baeldung/page/HomePageUITest.java
+++ src/test/java/com/baeldung/page/HomePageUITest.java
@@ -35,7 +35,7 @@ public final class HomePageUITest extends BaseUITest {
 
     @Test
     @Tag(GlobalConstants.TAG_SINGLE_URL)
-    public final void givenJaveWeeklyLinkClickedOnHomePage_whenSubscriptionPopupLoads_thenEmailAndSubscribeElementsExist() throws InterruptedException {
+    public final void givenJaveWeeklyLinkClickedOnHomePage_whenSubscriptionPopupLoads_thenItContainsEmailAndSubscribeElements() throws InterruptedException {
         homePageDriver.loadUrl();
         homePageDriver.clickNewsletterButton();
         Thread.sleep(1000);
