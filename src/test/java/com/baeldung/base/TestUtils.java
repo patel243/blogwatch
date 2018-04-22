e172262b413a3d2a8b58b4a25d84a14ae01dcf30
asif-anwar <asifanwar451@gmail.com>
2018-04-23 00:15:59 +0400

    fix for the test

--- src/test/java/com/baeldung/base/TestUtils.java
+++ src/test/java/com/baeldung/base/TestUtils.java
@@ -2,12 +2,17 @@ package com.baeldung.base;
 
 import java.util.List;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import com.baeldung.site.base.SitePage;
 
 public class TestUtils {
 
     public static boolean articleLinkFoundOnGitHubModule(List<String> gitHubModuleLinks, String articleRelativeURL, SitePage page) {
         boolean articleLinkFoundOnGitHubModule = false;
+        if (CollectionUtils.isEmpty(gitHubModuleLinks)) {
+            return true;
+        }
         for (String url : gitHubModuleLinks) {
             page.getWebDriver().get(url); // load GitHub module URL
             if (page.linkExistsInthePage(articleRelativeURL)) {
