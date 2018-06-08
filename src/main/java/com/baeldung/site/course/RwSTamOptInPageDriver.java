f7b8a1f0e02390d25e0e3e9894835197a5b37f49
asif-anwar <asifanwar451@gmail.com>
2018-06-08 18:17:49 +0400

    BAEL-6865 UI test

---
+++ src/main/java/com/baeldung/site/course/RwSTamOptInPageDriver.java
@@ -0,0 +1,43 @@
+package com.baeldung.site.course;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.baeldung.site.base.BlogBaseDriver;
+
+@Component
+public class RwSTamOptInPageDriver extends BlogBaseDriver {
+
+    @Override
+    @Value("${rws.team.optin.url}")
+    protected void setUrl(String url) {
+        this.url = this.getBaseURL() + url;
+    }
+
+    public void clickOnGetAccessLinkforSmallTeam() {
+        this.getWebDriver().findElement(By.xpath("//a[contains(@onclick,'buy_team_small')]")).click();
+    }
+
+    public void closePopupOnRwSTeamOptInPage() {
+        this.getWebDriver().findElement(By.xpath("//div[contains(@class, 'tve_ea_thrive_leads_form_close')]")).click();
+    }
+
+    public boolean theSubmitButtonOnthePopupisDisplayed() {
+        try {
+            return this.getWebDriver().findElement(By.xpath("//button[contains(., 'I HAVE QUESTIONS')]")).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public void clickOnGetAccessLinkforMediumTeam() {
+        this.getWebDriver().findElement(By.xpath("//a[contains(@onclick,'buy_team_medium')]")).click();
+    }
+
+    public void clickOnGetAccessLinkforLargeTeam() {
+        this.getWebDriver().findElement(By.xpath("//a[contains(@onclick,'buy_team_large')]")).click();
+    }
+
+}
---
+++ src/test/java/com/baeldung/page/RwSTeamOptInPageUITest.java
@@ -0,0 +1,40 @@
+package com.baeldung.page;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.baeldung.base.BaseUITest;
+import com.baeldung.config.GlobalConstants;
+import com.baeldung.site.course.RwSTamOptInPageDriver;
+
+public class RwSTeamOptInPageUITest extends BaseUITest {
+
+    @Autowired
+    private RwSTamOptInPageDriver rwsTamOptInPageDriver;
+
+    @Test
+    @Tag(GlobalConstants.TAG_HOURLY)
+    public final void givenOnTheRwSTeamOptInPage_whenTheGetAccessButtonIsClicked_thenTheOptInsPopupsWorkFine() throws InterruptedException {
+
+        rwsTamOptInPageDriver.loadUrl();
+
+        rwsTamOptInPageDriver.clickOnGetAccessLinkforSmallTeam();
+        Thread.sleep(1000);
+        assertTrue("Problem with opt-in pop-up for small team", rwsTamOptInPageDriver.theSubmitButtonOnthePopupisDisplayed());
+        rwsTamOptInPageDriver.closePopupOnRwSTeamOptInPage();
+
+        rwsTamOptInPageDriver.clickOnGetAccessLinkforMediumTeam();
+        Thread.sleep(1000);
+        assertTrue("Problem with opt-in pop-up for medium team", rwsTamOptInPageDriver.theSubmitButtonOnthePopupisDisplayed());
+        rwsTamOptInPageDriver.closePopupOnRwSTeamOptInPage();
+
+        rwsTamOptInPageDriver.clickOnGetAccessLinkforLargeTeam();
+        Thread.sleep(1000);
+        assertTrue("Problem with opt-in pop-up for medium team", rwsTamOptInPageDriver.theSubmitButtonOnthePopupisDisplayed());
+        rwsTamOptInPageDriver.closePopupOnRwSTeamOptInPage();
+
+    }
+}
