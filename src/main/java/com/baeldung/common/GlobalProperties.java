8c570fbff6423004b15165c8de446f3d4f3c7972
asif-anwar <asifanwar451@gmail.com>
2018-11-26 20:32:14 +0400

    small changes

--- src/main/java/com/baeldung/common/GlobalProperties.java
+++ src/main/java/com/baeldung/common/GlobalProperties.java
@@ -7,28 +7,27 @@ import java.util.Map;
 
 import org.yaml.snakeyaml.Yaml;
 
-import com.baeldung.util.Utils;
-
 public class GlobalProperties {
-    
+
     public static Map<String, List<String>> properties = new HashMap<>();
 
     static {
-        Yaml yaml = new Yaml();
-        InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream("ignore-list.yaml");        
-                 
-        properties =  yaml.load(inputStream);
-        
-         
-       /* for(Map.Entry<String, List<String>> entry:properties.entrySet()) {
+        try {
+            Yaml yaml = new Yaml();
+            InputStream inputStream = GlobalProperties.class.getClassLoader().getResourceAsStream("ignore-list.yaml");
+            properties = yaml.load(inputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /* for(Map.Entry<String, List<String>> entry:properties.entrySet()) {
             System.out.println("---------------------------");
             System.out.println(entry.getKey());
             for(String s: entry.getValue()) {
                 System.out.println(s);
             }
         }*/
-        
-      
+
     }
-   
+
 }
