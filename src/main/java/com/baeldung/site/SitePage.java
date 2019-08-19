81b1c8a959a4626726b6592bb6f591dbafd1c264
asif-anwar <asifanwar451@gmail.com>
2019-08-19 17:55:30 +0400

    small changes

--- src/main/java/com/baeldung/site/SitePage.java
+++ src/main/java/com/baeldung/site/SitePage.java
@@ -117,31 +117,38 @@ public class SitePage extends BlogBaseDriver {
 
     public List<WebElement> findImagesPointingToInvalidEnvOnTheArticle() {
         String baseURLWithOutHttp = this.getBaseURL().substring(6);
-        return this.getWebDriver().findElements(By.xpath("//section//img[( contains(@src, 'www.') or contains(@src, 'http:') or contains(@src, 'https:') ) and not(contains(@src, '" + this.getBaseURL() + "') or contains(@src, '"
-                + GlobalConstants.BAELDUNG_HOME_PAGE_URL_WITHOUT_WWW_PREFIX + "') or contains(@src, '" + baseURLWithOutHttp + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(0) + "')) and not(contains(@src, '"
-                + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(1) + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(2) + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(3) + "')) and not(contains(@src, '"
-                + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(4) + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(5) + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(6) + "'))]"));
+        return this.getWebDriver()
+                .findElements(By.xpath("//section//img[( contains(@src, 'www.') or contains(@src, 'http:') or contains(@src, 'https:') ) and not(contains(@src, '" + this.getBaseURL() + "') or contains(@src, '"
+                        + GlobalConstants.BAELDUNG_HOME_PAGE_URL_WITHOUT_WWW_PREFIX + "') or contains(@src, '" + baseURLWithOutHttp + "')) and not(contains(@src, '" + GlobalConstants.BAELDUNG_HOME_PAGE_URL_WITH_WWW_PREFIX + "')) and not(contains(@src, '"
+                        + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(0) + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(1) + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(2)
+                        + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(3) + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(4) + "')) and not(contains(@src, '"
+                        + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(5) + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(6) + "'))]"));
     }
 
     public List<WebElement> findAnchorsPointingToAnImageAndInvalidEnvOnTheArticle() {
         String baseURLWithOutHttp = this.getBaseURL().substring(6);
-        return this.getWebDriver().findElements(
-                By.xpath("//section//a[( contains(@href, 'www.') or contains(@href, 'http:') or contains(@href, 'https:') ) and ( contains(@href, '.jpg') or contains(@href, '.jpeg') or contains(@href, '.png')) and not(contains(@href, '" + this.getBaseURL()
-                        + "') or contains(@href, '" + GlobalConstants.BAELDUNG_HOME_PAGE_URL_WITHOUT_WWW_PREFIX + "') or contains(@href, '" + baseURLWithOutHttp + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(0)
-                        + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(1) + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(2) + "')) and not(contains(@href, '"
-                        + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(3) + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(4) + "'))  and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(5) + "'))]"));
+        return this.getWebDriver()
+                .findElements(By.xpath("//section//a[( contains(@href, 'www.') or contains(@href, 'http:') or contains(@href, 'https:') ) and ( contains(@href, '.jpg') or contains(@href, '.jpeg') or contains(@href, '.png')) and not(contains(@href, '"
+                        + this.getBaseURL() + "') or contains(@href, '" + GlobalConstants.BAELDUNG_HOME_PAGE_URL_WITHOUT_WWW_PREFIX + "') or contains(@href, '" + baseURLWithOutHttp + "')) and not(contains(@href, '"
+                        + GlobalConstants.BAELDUNG_HOME_PAGE_URL_WITH_WWW_PREFIX + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(0) + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(1)
+                        + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(2) + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(3) + "')) and not(contains(@href, '"
+                        + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(4) + "'))  and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(5) + "'))]"));
     }
 
     public List<WebElement> findImagesPointingToInvalidEnvOnThePage() {
         String baseURLWithOutHttp = this.getBaseURL().substring(6);
-        return this.getWebDriver().findElements(By.xpath("//article//img[( contains(@src, 'www.') or contains(@src, 'http:')  or contains(@src, 'https:') ) and not(contains(@src, '" + this.getBaseURL() + "') or contains(@src, '" + baseURLWithOutHttp
-                + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(0) + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(1) + "'))]"));
+        return this.getWebDriver()
+                .findElements(By.xpath("//article//img[( contains(@src, 'www.') or contains(@src, 'http:')  or contains(@src, 'https:') ) and not(contains(@src, '" + this.getBaseURL() + "') or contains(@src, '" + baseURLWithOutHttp
+                        + "')) and not(contains(@src, '" + GlobalConstants.BAELDUNG_HOME_PAGE_URL_WITH_WWW_PREFIX + "')) and not(contains(@src, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(0) + "')) and not(contains(@src, '"
+                        + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(1) + "'))]"));
     }
 
     public List<WebElement> findAnchorsPointingToAnImageAndInvalidEnvOnThePage() {
         String baseURLWithOutHttp = this.getBaseURL().substring(6);
-        return this.getWebDriver().findElements(By.xpath("//article//a[( contains(@href, 'www.') or contains(@href, 'http:')  or contains(@href, 'https:') ) and not(contains(@href, '" + this.getBaseURL() + "') or contains(@href, '" + baseURLWithOutHttp
-                + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(0) + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(1) + "'))]"));
+        return this.getWebDriver()
+                .findElements(By.xpath("//article//a[( contains(@href, 'www.') or contains(@href, 'http:')  or contains(@href, 'https:') ) and not(contains(@href, '" + this.getBaseURL() + "') or contains(@href, '" + baseURLWithOutHttp
+                        + "')) and not(contains(@src, '" + GlobalConstants.BAELDUNG_HOME_PAGE_URL_WITH_WWW_PREFIX + "')) and not(contains(@href, '" + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(0) + "')) and not(contains(@href, '"
+                        + GlobalConstants.DOMAIN_LIST_TO_EXCLUDE.get(1) + "'))]"));
     }
 
     public boolean findMetaDescriptionTag() {
