ea22fb87cabd44af2ebc81e8ac32b0e4727564da
asif-anwar <asifanwar451@gmail.com>
2020-02-08 15:38:14 +0400

    small changes

--- src/test/java/com/baeldung/common/BaseTest.java
+++ src/test/java/com/baeldung/common/BaseTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,12 +14,7 @@ public class BaseTest {
 
     protected Logger logger = LoggerFactory.getLogger(getClass());
     protected static Logger baseLogger = LoggerFactory.getLogger(BaseTest.class);
-    protected static AtomicInteger failedTests = new AtomicInteger();
-
-    @BeforeAll
-    public static void init() {
-        failedTests.set(0);
-    }
+    protected static AtomicInteger failedTests = new AtomicInteger(0);   
 
     protected static void recordMetrics(int count, TestMetricTypes metricType) {
         if (metricType.equals(TestMetricTypes.FAILED)) {
