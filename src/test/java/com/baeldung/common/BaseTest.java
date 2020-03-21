21ee01c5c0195cfd50f419bf9c93d9e2a9b85afa
asif-anwar <asifanwar451@gmail.com>
2020-03-21 14:00:16 +0400

    loggin test name

--- src/test/java/com/baeldung/common/BaseTest.java
+++ src/test/java/com/baeldung/common/BaseTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.fail;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +16,7 @@ public class BaseTest {
 
     protected Logger logger = LoggerFactory.getLogger(getClass());
     protected static Logger baseLogger = LoggerFactory.getLogger(BaseTest.class);
-    protected static AtomicInteger failedTests = new AtomicInteger(0);   
+    protected static AtomicInteger failedTests = new AtomicInteger(0);
 
     protected static void recordMetrics(int count, TestMetricTypes metricType) {
         if (metricType.equals(TestMetricTypes.FAILED)) {
@@ -31,6 +33,13 @@ public class BaseTest {
 
     protected void failTestWithLoggingTotalNoOfFailures(String fialureMessage) {
         fail(fialureMessage + Utils.messageForTotalNoOfFailuresAtTheTestLevel(getMetrics(TestMetricTypes.FAILED)));
+    }
+
+    @BeforeEach
+    public final void logTestname(TestInfo testInfo) {
+        logger.info("-----------------------------------------------------------------------");
+        logger.info("Executing: {}", testInfo.getDisplayName());
+        logger.info("-----------------------------------------------------------------------");
     }
 
     @AfterAll
