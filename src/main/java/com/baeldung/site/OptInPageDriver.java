d5547f236c4ed7fa255afc0c2b8b2b1e22c1c761
asif-anwar <asifanwar451@gmail.com>
2020-05-08 15:08:13 +0400

    for JAVA-1683

---
+++ src/main/java/com/baeldung/site/OptInPageDriver.java
@@ -0,0 +1,83 @@
+package com.baeldung.site;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OptInPageDriver extends BlogBaseDriver {
+
+    public void clickOnGetAccessLinkforSmallTeam() throws InterruptedException {
+        logger.info("executing clickOnGetAccessLinkforSmallTeam()");        
+        acceptCookie();   
+        Thread.sleep(5000);
+        WebDriverWait wait = new WebDriverWait(this.getWebDriver(), 20);
+        WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'buy_team_small')]/a")));
+        button.click();
+    }
+
+    public void closePopupOnRwSTeamOptInPage() {
+        logger.info("executing closePopupOnRwSTeamOptInPage()");        
+        for (WebElement element : this.getWebDriver().findElements(By.xpath("//div[contains(@class, 'tve_ea_thrive_leads_form_close')]"))) {           
+            if (element.isDisplayed()) {
+                element.click();
+                logger.info("Popup closed");
+                break;
+            }
+        }       
+    }
+
+    public boolean theSubmitButtonOnthePopupisDisplayed() throws InterruptedException {                 
+        logger.info("executing theSubmitButtonOnthePopupisDisplayed()");        
+        WebDriverWait wait = new WebDriverWait(this.getWebDriver(), 20);
+        WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[contains(., 'WE MIGHT BE INTERESTED')])")));
+        return button.isDisplayed();       
+    }
+
+    public void clickOnGetAccessLinkforMediumTeam() throws InterruptedException {
+        logger.info("executing clickOnGetAccessLinkforMediumTeam()"); 
+        Thread.sleep(5000);
+        WebDriverWait wait = new WebDriverWait(this.getWebDriver(), 20);
+        WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'buy_team_medium')]/a")));
+        button.click();
+        
+    }
+
+    public void clickOnGetAccessLinkforLargeTeam() throws InterruptedException {
+        logger.info("executing clickOnGetAccessLinkforLargeTeam()");
+        Thread.sleep(5000);
+        WebDriverWait wait = new WebDriverWait(this.getWebDriver(), 20);
+        WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'buy_team_large')]/a")));
+        button.click();        
+    }
+
+    public void closeChatPopupIfOpen() {
+        try {
+            JavascriptExecutor js = ((JavascriptExecutor) this.getWebDriver());
+            // logger.info("is_Crawler variable.................." + js.executeScript("tve_dash_front.is_crawler"));
+            js.executeScript("document.getElementById('drift-widget-container').style.display = 'none';");
+            js.executeScript("document.getElementById('drift-widget').style.display = 'none';");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void acceptCookie() {
+        try {            
+            JavascriptExecutor js = ((JavascriptExecutor) this.getWebDriver());
+            js.executeScript("document.getElementById('cn-accept-cookie').click();");
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void setUrl(String url) {
+        this.url = this.getBaseURL() + url;
+    }
+
+}
---
+++ src/test/java/com/baeldung/selenium/page/OptInPopupUITest.java
@@ -0,0 +1,82 @@
+package com.baeldung.selenium.page;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.baeldung.selenium.common.BaseUISeleniumTest;
+import com.baeldung.site.OptInPageDriver;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class OptInPopupUITest extends BaseUISeleniumTest {
+    @Autowired
+    private OptInPageDriver optInPageDriver;
+
+    @Value("${team.optin.urls}")
+    private String[] optInUrls;
+    
+    @Value("${team.optin.test.retry}")
+    private int retry;
+
+    int retryCount = 0;
+
+    @BeforeEach
+    public void loadNewWindow() throws IOException {
+        page.openNewWindow();
+        retryCount = 0;
+    }
+
+    @ParameterizedTest(name = " {displayName} - Test popups on {0} ")
+    @MethodSource("optInPopusTestDataProvider")
+    @Tag("optinPopsTest")
+    public final void givenAPageWithOptins_whenThePopupsAreOpened_thenTheOptInsPopupsWorkFine(String optInPagUrl) {
+        optInPageDriver.setUrl(optInPagUrl);
+        try {
+            optInPageDriver.loadUrl();
+            Thread.sleep(5000);
+
+            optInPageDriver.clickOnGetAccessLinkforSmallTeam();
+            assertTrue(optInPageDriver.theSubmitButtonOnthePopupisDisplayed(), "Problem with opt-in pop-up for small team");
+            optInPageDriver.closePopupOnRwSTeamOptInPage();
+
+            optInPageDriver.clickOnGetAccessLinkforMediumTeam();
+            assertTrue(optInPageDriver.theSubmitButtonOnthePopupisDisplayed(), "Problem with opt-in pop-up for medium team");
+            optInPageDriver.closePopupOnRwSTeamOptInPage();
+
+            optInPageDriver.clickOnGetAccessLinkforLargeTeam();
+            assertTrue(optInPageDriver.theSubmitButtonOnthePopupisDisplayed(), "Problem with opt-in pop-up for Large team");
+            optInPageDriver.closePopupOnRwSTeamOptInPage();
+
+        } catch (Exception e) {
+            if (retry == retryCount) {
+                logger.debug(retry + " retries completed.");
+                logger.error(e.getMessage());
+                fail(e.getMessage());
+            } else {
+                retryCount++;
+                logger.error(e.getMessage());
+                givenAPageWithOptins_whenThePopupsAreOpened_thenTheOptInsPopupsWorkFine(optInPagUrl);
+
+            }
+        }
+
+    }
+
+    public Stream<Arguments> optInPopusTestDataProvider() {
+        return Arrays.stream(optInUrls).map(url -> Arguments.of(url));
+    }
+
+}
