498b8b67ac113ef0ff1460a21faf2bcb47aef8c3
asif-anwar <asifanwar451@gmail.com>
2020-05-30 11:33:29 +0400

    adding the test to the daily build

--- src/test/java/com/baeldung/selenium/common/CommonUITest.java
+++ src/test/java/com/baeldung/selenium/common/CommonUITest.java
@@ -423,6 +423,7 @@ public class CommonUITest extends BaseUISeleniumTest {
     @ParameterizedTest(name = " {displayName} - verify footer links on {0}")
     @MethodSource("com.baeldung.utility.TestUtils#footerLinksTestDataProvider()")
     @Tag(GlobalConstants.TAG_SITE_SMOKE_TEST)
+    @Tag(GlobalConstants.TAG_DAILY)
     public final void givenURLsWithFooterLinks_whenAnaysingFooterLinks_thenTheAnchorTestAndAnchorLinksExist(String url, String footerTag, List<FooterLinksDataVO.link> footerLinks) throws JsonProcessingException, IOException {
 
         page.setUrl(page.getBaseURL() + url);
